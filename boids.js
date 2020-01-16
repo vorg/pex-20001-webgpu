5498e820d037914235dc26fb55cc36e1fdf604c1
Marcin Ignac <marcin.ignac@gmail.com>
2020-01-16 15:50:50 +0000

    [boids] remove unused code

--- boids.js
+++ boids.js
@@ -130,8 +130,6 @@ const fragmentShaderGLSL = `
     fragColor = vColor;
   }`;
 
-
-
 async function init() {
   const ctx = await createContext({ width: 600, height: 600 });
   document.body.style.margin = 0
@@ -151,14 +149,6 @@ async function init() {
     element: ctx.canvas
   });
 
-  /* old */
-  // const computeBindGroupLayout = device.createBindGroupLayout({
-  //   bindings: [
-  //     { binding: 0, visibility: GPUShaderStage.COMPUTE, type: "uniform-buffer" },
-  //     { binding: 1, visibility: GPUShaderStage.COMPUTE, type: "storage-buffer" },
-  //     { binding: 2, visibility: GPUShaderStage.COMPUTE, type: "storage-buffer" },
-  //   ],
-  // });
   const computeBindGroupLayout = ctx.bindGroupLayout([
     {
       visibility: ctx.ShaderStage.Compute,
@@ -170,72 +160,6 @@ async function init() {
     },
     { visibility: ctx.ShaderStage.Compute, type: ctx.BindingType.StorageBuffer }
   ]);
-
-  // const renderPipeline = device.createRenderPipeline({
-  //   layout: device.createPipelineLayout({ bindGroupLayouts: [] }),
-
-  //   vertexStage: {
-  //     module: device.createShaderModule({
-  //       code: glslang.compileGLSL(vertexShaderGLSL, "vertex"),
-
-  //       // @ts-ignore
-  //       source: vertexShaderGLSL,
-  //       transform: source => glslang.compileGLSL(source, "vertex"),
-  //     }),
-  //     entryPoint: "main"
-  //   },
-  //   fragmentStage: {
-  //     module: device.createShaderModule({
-  //       code: glslang.compileGLSL(fragmentShaderGLSL, "fragment"),
-
-  //       // @ts-ignore
-  //       source: fragmentShaderGLSL,
-  //       transform: source => glslang.compileGLSL(source, "fragment"),
-  //     }),
-  //     entryPoint: "main"
-  //   },
-
-  //   primitiveTopology: "triangle-list",
-
-  //   depthStencilState: {
-  //     depthWriteEnabled: true,
-  //     depthCompare: "less",
-  //     format: "depth24plus-stencil8",
-  //   },
-
-  //   vertexState: {
-  //     vertexBuffers: [{
-  //       // instanced particles buffer
-  //       arrayStride: 4 * 4,
-  //       stepMode: "instance",
-  //       attributes: [{
-  //         // instance position
-  //         shaderLocation: 0,
-  //         offset: 0,
-  //         format: "float2"
-  //       }, {
-  //         // instance velocity
-  //         shaderLocation: 1,
-  //         offset: 2 * 4,
-  //         format: "float2"
-  //       }],
-  //     }, {
-  //       // vertex buffer
-  //       arrayStride: 2 * 4,
-  //       stepMode: "vertex",
-  //       attributes: [{
-  //         // vertex positions
-  //         shaderLocation: 2,
-  //         offset: 0,
-  //         format: "float2"
-  //       }],
-  //     }],
-  //   },
-
-  //   colorStates: [{
-  //     format: "bgra8unorm",
-  //   }],
-  // });
 
   const pipeline = ctx.pipeline({
     vert: vertexShaderGLSL,
@@ -286,62 +210,22 @@ async function init() {
     }
   });
 
-  // const computePipeline = device.createComputePipeline({
-  //   layout: computePipelineLayout,
-  //   computeStage: {
-  //     module: device.createShaderModule({
-  //       code: glslang.compileGLSL(computeShaderGLSL, "compute"),
-
-  //       // @ts-ignore
-  //       source: computeShaderGLSL,
-  //       transform: source => glslang.compileGLSL(source, "compute"),
-  //     }),
-  //     entryPoint: "main"
-  //   },
-  // });
-
   const computePipeline = ctx.computePipeline({
     compute: computeShaderGLSL,
     bindGroupLayouts: [computeBindGroupLayout]
   });
 
-  // const depthTexture = device.createTexture({
-  //   size: { width: canvas.width, height: canvas.height, depth: 1 },
-  //   format: "depth24plus-stencil8",
-  //   usage: GPUTextureUsage.OUTPUT_ATTACHMENT
-  // });
   const depthTexture = ctx.texture({
     width: ctx.canvas.width,
     height: ctx.canvas.height,
     format: ctx.PixelFormat.Depth24PlusStencil18
   });
 
-  // const renderPassDescriptor = {
-  //   colorAttachments: [{
-  //     attachment: undefined,  // Assigned later
-  //     loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-  //   }],
-  //   depthStencilAttachment: {
-  //     attachment: depthTexture.createView(),
-  //     depthLoadValue: 1.0,
-  //     depthStoreOp: "store",
-  //     stencilLoadValue: 0,
-  //     stencilStoreOp: "store",
-  //   }
-  // };
-
   const pass = ctx.pass({
     clearColor: [0, 0, 0, 1],
     depth: depthTexture,
     clearDepth: 1
   });
-
-  // const vertexBufferData = new Float32Array([-0.01, -0.02, 0.01, -0.02, 0.00, 0.02]);
-  // const verticesBuffer = device.createBuffer({
-  //   size: vertexBufferData.byteLength,
-  //   usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-  // });
-  // verticesBuffer.setSubData(0, vertexBufferData);
 
   let verticesBuffer = ctx.vertexBuffer({
     data: [-0.01, -0.02, 0.01, -0.02, 0.0, 0.02].map((f) => f * 0.5)
@@ -357,11 +241,6 @@ async function init() {
     0.05, // rule2Scale;
     0.005 // rule3Scale;
   ]);
-  // const simParamBuffer = device.createBuffer({
-  //   size: simParamData.byteLength,
-  //   usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-  // });
-  // simParamBuffer.setSubData(0, simParamData);
 
   const simParamBuffer = ctx.uniformBuffer({
     size: simParamData.byteLength
@@ -374,8 +253,6 @@ async function init() {
     var v = Math.random()
     initialParticleData[8 * i + 0] = 2 * (u - 0.5);
     initialParticleData[8 * i + 1] = 2 * (v - 0.5);
-    // initialParticleData[8 * i + 2] = 2 * (Math.random() - 0.5) * 0.1;
-    // initialParticleData[8 * i + 3] = 2 * (Math.random() - 0.5) * 0.1;
     initialParticleData[8 * i + 2] = random.float(0.1, 0.3);
     initialParticleData[8 * i + 3] = random.float(0.1, 0.3);
     initialParticleData[8 * i + 4] = u
@@ -387,11 +264,6 @@ async function init() {
   const particleBuffers = new Array(2);
   const particleBindGroups = new Array(2);
   for (let i = 0; i < 2; ++i) {
-    // particleBuffers[i] = device.createBuffer({
-    //   size: initialParticleData.byteLength,
-    //   usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.VERTEX | GPUBufferUsage.STORAGE
-    // });
-    //particleBuffers[i].setSubData(0, initialParticleData);
     particleBuffers[i] = ctx.vertexBuffer({
       data: initialParticleData,
       usage:
@@ -400,31 +272,6 @@ async function init() {
   }
 
   for (let i = 0; i < 2; ++i) {
-    // particleBindGroups[i] = device.createBindGroup({
-    //   layout: computeBindGroupLayout,
-    //   bindings: [{
-    //     binding: 0,
-    //     resource: {
-    //       buffer: simParamBuffer,
-    //       offset: 0,
-    //       size: simParamData.byteLength
-    //     },
-    //   }, {
-    //     binding: 1,
-    //     resource: {
-    //       buffer: particleBuffers[i],
-    //       offset: 0,
-    //       size: initialParticleData.byteLength,
-    //     },
-    //   }, {
-    //     binding: 2,
-    //     resource: {
-    //       buffer: particleBuffers[(i + 1) % 2],
-    //       offset: 0,
-    //       size: initialParticleData.byteLength,
-    //     },
-    //   }],
-    // });
     particleBindGroups[i] = ctx.bindGroup({
       layout: computeBindGroupLayout,
       bindings: [
@@ -446,32 +293,6 @@ async function init() {
       ]
     });
   }
-
-  // let t = 0;
-  // return function frame() {
-  //   renderPassDescriptor.colorAttachments[0].attachment = swapChain.getCurrentTexture().createView();
-
-  //   const commandEncoder = device.createCommandEncoder({});
-  //   {
-  //     const passEncoder = commandEncoder.beginComputePass();
-  //     passEncoder.setPipeline(computePipeline);
-  //     passEncoder.setBindGroup(0, particleBindGroups[t % 2]);
-  //     passEncoder.dispatch(numParticles);
-  //     passEncoder.endPass();
-  //   }
-
-  //   {
-  //     const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-  //     passEncoder.setPipeline(renderPipeline);
-  //     passEncoder.setVertexBuffer(0, particleBuffers[(t + 1) % 2]);
-  //     passEncoder.setVertexBuffer(1, verticesBuffer);
-  //     passEncoder.draw(3, numParticles, 0, 0);
-  //     passEncoder.endPass();
-  //   }
-  //   device.defaultQueue.submit([commandEncoder.finish()]);
-
-  //   ++t;
-  // }
 
   const drawCmd = {
     pass: pass,
